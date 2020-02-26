e1fbdb0e0ad766567718eda4f003a1a980f3f5ae
Sachin Gwasikoti <sachingw777@gmail.com>
2020-02-26 21:42:56 +0530

    add delete

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,7 +1,13 @@
- //Strikethrough specific todos with click
-
-$("li").click(function(){
+ //Strikethrough specific todos with click 
+$("ul").on("click", "li", function(){
 	$(this).toggleClass("completed");
 });
 
+//click on x to delete Todo
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
 
